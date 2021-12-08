8d3ca28c208e88c71df8b6524a278e979ccf3f0d
Leandrei Sagun <71297137+lssagun2@users.noreply.github.com>
2021-12-08 18:23:00 +0800

    Functional basic gameplay

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import pygame
 import time
+import math
 from pygame.locals import *
 
 
@@ -15,8 +16,8 @@ except socket.error as e:
 	print(str(e))
 
 #game variables
-MAP_WIDTH = 2000
-MAP_HEIGHT = 2000
+MAP_WIDTH = 2500
+MAP_HEIGHT = 2500
 USERNAME = "asdasd"
 
 #colors
@@ -26,58 +27,59 @@ PLAYER_COLOR = (7,135,123)
 
 #classes
 class Player(pygame.sprite.Sprite):
-	def __init__(self, ID, username, color, radius, speed):
+	def __init__(self, ID, username, location, health, color, radius, speed):
 		pygame.sprite.Sprite.__init__(self)
 		self.ID = ID
 		self.username = username
-		self.health = 100
-		self.speed = 1
 		self.radius = radius
+		self.health = health
 		self.speed = speed
+		self.time_last_fired = 0
+		self.fire_rate = 5 		#bullets per second
+		self.auto_firing = True	#True if automatic, False if manual
 		self.image = pygame.Surface([2*radius, 2*radius], pygame.SRCALPHA)
 		self.rect = pygame.draw.circle(self.image, color, (radius, radius), radius)
-	def move(self, pressed):
-		if pressed[pygame.K_RSHIFT] or pressed[pygame.K_LSHIFT]:
+		self.rect.center = location
+	def update(self, location, health):
+		self.rect.center = location
+		self.health = health
+	def move(self, key_pressed):
+		if key_pressed[pygame.K_RSHIFT] or key_pressed[pygame.K_LSHIFT]:
 			self.speed = 2
 		else:
 			self.speed = 3
-		if pressed[pygame.K_w] or pressed[pygame.K_UP]:
-			if self.rect.y - self.speed < 0:
-				self.rect.move_ip(0, -self.rect.y)
-			else:
-				self.rect.move_ip(0, -self.speed)
+		if key_pressed[pygame.K_w] or key_pressed[pygame.K_UP]:
+			self.rect.move_ip(0, -self.speed)
 			hits = pygame.sprite.spritecollide(self, covers, False)
 			if len(hits) != 0:
 				self.rect.move_ip(0, hits[0].rect.bottom - self.rect.top)
-		if pressed[pygame.K_s] or pressed[pygame.K_DOWN]:
-			if self.rect.y + 2 * self.radius + self.speed > MAP_HEIGHT:
-				self.rect.move_ip(0, MAP_HEIGHT - self.rect.y - 2 * self.radius)
-			else:
-				self.rect.move_ip(0, self.speed)
+		if key_pressed[pygame.K_s] or key_pressed[pygame.K_DOWN]:
+			self.rect.move_ip(0, self.speed)
 			hits = pygame.sprite.spritecollide(self, covers, False)
 			if len(hits) != 0:
 				self.rect.move_ip(0, hits[0].rect.top - self.rect.bottom)
-		if pressed[pygame.K_a] or pressed[pygame.K_LEFT]:
-			if self.rect.x - self.speed < 0:
-				self.rect.move_ip(-self.rect.x, 0)
-			else:
-				self.rect.move_ip(-self.speed, 0)
+		if key_pressed[pygame.K_a] or key_pressed[pygame.K_LEFT]:
+			self.rect.move_ip(-self.speed, 0)
 			hits = pygame.sprite.spritecollide(self, covers, False)
 			if len(hits) != 0:
 				self.rect.move_ip(hits[0].rect.right - self.rect.left, 0)
-		if pressed[pygame.K_d] or pressed[pygame.K_RIGHT]:
-			if self.rect.x + 2 * self.radius + self.speed  > MAP_WIDTH:
-				self.rect.move_ip(MAP_WIDTH - self.rect.x - 2 * self.radius, 0)
-			else:
-				self.rect.move_ip(self.speed, 0)
+		if key_pressed[pygame.K_d] or key_pressed[pygame.K_RIGHT]:
+			self.rect.move_ip(self.speed, 0)
 			hits = pygame.sprite.spritecollide(self, covers, False)
 			if len(hits) != 0:
 				self.rect.move_ip(hits[0].rect.left - self.rect.right, 0)
 class Cover(pygame.sprite.Sprite):
-	def __init__(self, color, width, height):
+	def __init__(self, color, location, width, height):
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.Surface([width, height])
 		self.rect = self.image.get_rect()
+		self.rect.x, self.rect.y = location
+class Bullet(pygame.sprite.Sprite):
+	def __init__(self, location, radius = 3):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface([2*radius, 2*radius], pygame.SRCALPHA)
+		self.rect = pygame.draw.circle(self.image, (127, 127, 127), (radius, radius), radius)
+		self.rect.center = location
 
 #create main display
 screen = pygame.display.set_mode((500, 500))
@@ -86,14 +88,12 @@ map_surface = pygame.Surface((MAP_WIDTH, MAP_HEIGHT))
 map_surface = map_surface.convert()
 
 #receive data about covers from server and create Cover objects
-covers_data = client_socket.recv(4096)
+covers_data = client_socket.recv(8192)
 covers_data = eval(covers_data.decode('utf-8'))
 covers = pygame.sprite.Group()
 for cover_data in covers_data:
-	x, y, width, height = cover_data
-	cover = Cover(BLACK, width, height)
-	cover.rect.x = x
-	cover.rect.y = y
+	location, width, height = cover_data
+	cover = Cover(BLACK, location, width, height)
 	covers.add(cover)
 
 #communicate with server about player information (id and initial location) of this client
@@ -101,93 +101,113 @@ my_username = input("Please input username: ")
 client_socket.send(str.encode(my_username))
 my_info = client_socket.recv(2048)
 my_info = eval(my_info.decode('utf-8'))
-my_id, location = my_info
+my_id, my_location, my_health = my_info
+client_socket.send(str.encode("received"))
 
 #create Player object for this client
 players = pygame.sprite.Group()
-me = Player(my_id, my_username, PLAYER_COLOR, 7, 3)
-me.rect.x = location[0]
-me.rect.y = location[1]
+alive_players = pygame.sprite.Group()
+dead_players = pygame.sprite.Group()
+me = Player(my_id, my_username, my_location, my_health, PLAYER_COLOR, 7, 3)
 players.add(me)
+alive_players.add(me)
 
 #get other information players
 other_players = client_socket.recv(8192)
-other_usernames, other_locations = eval(other_players.decode('utf-8'))
+other_players = eval(other_players.decode('utf-8'))
 
 #create Player objects for other players
-for i in range(len(other_locations)):
-	if i == my_id:
+for ID, values in other_players.items():
+	if ID == me.ID:
 		continue
-	if other_usernames[i] is None:
-		continue
-	username = other_usernames[i]
-	location = other_locations[i]
-	player = Player(i, username, PLAYER_COLOR, 7, 3)
-	player.rect.x = location[0]
-	player.rect.y = location[1]
-	players.add(player)
-for player in players.sprites():
-	print(player)
+	username, location, health = values
+	players.add(Player(ID, username, location, health, PLAYER_COLOR, 7, 3))
+	alive_players.add(Player(ID, username, location, health, PLAYER_COLOR, 7, 3))
+
 #initialize Pygame
 pygame.init()
 clock = pygame.time.Clock()
 
-
-
 #main loop
-pressed = {pygame.K_w: False, pygame.K_UP: False, pygame.K_s: False, pygame.K_DOWN: False, pygame.K_d: False, pygame.K_RIGHT: False, pygame.K_a: False, pygame.K_LEFT: False, K_RSHIFT: False, K_LSHIFT: False}
+key_pressed = {pygame.K_w: False, pygame.K_UP: False, pygame.K_s: False, pygame.K_DOWN: False, pygame.K_d: False, pygame.K_RIGHT: False, pygame.K_a: False, pygame.K_LEFT: False, K_RSHIFT: False, K_LSHIFT: False}
+mouse_pressed = {1: False, 2: False, 3: False, 4: False, 5: False}
 done = False
 while not done:
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			done = True
-		if event.type == KEYDOWN and event.key in pressed.keys():
-			pressed[event.key] = True
-		if event.type == KEYUP and event.key in pressed.keys():
-			pressed[event.key] = False
-	me.move(pressed)
-	client_socket.send(str.encode(str((me.rect.x, me.rect.y))))
-	other_players = client_socket.recv(8192)
-	other_usernames, other_locations = eval(other_players.decode('utf-8'))
-	for i in range(len(other_locations)):
-		if i == my_id:
-			continue
-		if other_locations[i] is None:
-			continue
-		location = other_locations[i]
-		username = other_usernames[i]
-		found = False
-		for player in players.sprites():
-			if player.ID != i:
-				continue
-			found = True
-			player.rect.x = location[0]
-			player.rect.y = location[1]
-		if not found:
-			new_player = Player(i, username, PLAYER_COLOR, 7, 3)
-			new_player.rect.x = location[0]
-			new_player.rect.y = location[1]
-			players.add(new_player)
+		if event.type == KEYDOWN and event.key in key_pressed.keys():
+			key_pressed[event.key] = True
+		if event.type == KEYUP and event.key in key_pressed.keys():
+			key_pressed[event.key] = False
+		if event.type == MOUSEBUTTONDOWN:
+			mouse_pressed[event.button] = True
+		if event.type == MOUSEBUTTONUP:
+			mouse_pressed[event.button] = False
+	me.move(key_pressed)
+	client_socket.send(str.encode(str(me.rect.center)))
+
+	#update other players' location
+	players_info = client_socket.recv(8192)
+	players_info = eval(players_info.decode('utf-8'))
+	IDs = list(players_info.keys())
+	values = list(players_info.values())
+	usernames, locations, healths = list(zip(*values))
+	locations = dict(zip(IDs, locations))
+	players_to_remove = []
+	for player in alive_players.sprites():
+		player.update(locations[player.ID], healths[player.ID])
+		if player.health <= 0:
+			alive_players.remove(player)
+			dead_players.add(player)
+	#create Player objects for players who just joined
+	if len(players.sprites()) != len(players_info):
+		new_players = [ID for ID in IDs if ID not in [player.ID for player in players.sprites()]]
+		for ID in new_players:
+			username, location, health = players_info[ID]
+			players.add(Player(ID, username, location, health, PLAYER_COLOR, 7, 3))
+			alive_players.add(Player(ID, username, location, health, PLAYER_COLOR, 7, 3))
+
+	#send bullet information to server
+	if mouse_pressed[1] == True:
+		current_time = time.time()
+		if current_time - me.time_last_fired > 1 / me.fire_rate:
+			origin = me.rect.center
+			x, y = pygame.mouse.get_pos()
+			direction = (x - 250, y - 250)
+			client_socket.send(str.encode(str((origin, direction, 10))))
+			me.time_last_fired = current_time
+		else:
+			client_socket.send(str.encode("empty"))
+	else:
+		client_socket.send(str.encode("empty"))
+
+	#receive bullet locations
+	bullets = pygame.sprite.Group()
+	bullets_location = client_socket.recv(16384)
+	bullets_location = eval(bullets_location.decode('utf-8'))
+	for location in bullets_location:
+		bullets.add(Bullet(location))
+
+
 	map_surface.fill(WHITE)
 	covers.draw(map_surface)
-	players.draw(map_surface)
-	left = me.rect.x - 250
-	if left < 0:
-		left = 0
-	elif left + 500 > MAP_WIDTH:
-		left = MAP_WIDTH - 500
-	top = me.rect.y - 250
-	if top < 0:
-		top = 0
-	elif top + 500 > MAP_HEIGHT:
-		top = MAP_HEIGHT - 500
+	alive_players.draw(map_surface)
+	bullets.draw(map_surface)
+	left, top = me.rect.center
+	left -= 250
+	top -= 250
 	screen.blit(map_surface, (0, 0), (left, top, 500, 500))
 	pygame.display.flip()
 	clock.tick(60)
-
+	players.remove(players_to_remove)
+	if me.health <= 0:
+		done = True
+		continue
 	# Input = input('Say Something: ')
 	# client_socket.send(str.encode(Input))
 	# Response = client_socket.recv(1024)
 	# print(Response.decode('utf-8'))
-
+print("Thanks for playing!")
 client_socket.close()
+pygame.quit()
--- server.py
+++ server.py
@@ -1,10 +1,62 @@
 import socket
 import os
 import random
+import math
 from _thread import *
+import pygame
+from pygame.locals import *
 
-MAP_WIDTH = 2000
-MAP_HEIGHT = 2000
+class Player(pygame.sprite.Sprite):
+	def __init__(self, ID, username, location):
+		pygame.sprite.Sprite.__init__(self)
+		self.ID = ID
+		self.username = username
+		self.health = 100
+		self.location = location
+		self.image = pygame.Surface([2*7, 2*7], pygame.SRCALPHA)
+		self.rect = pygame.draw.circle(self.image, (7,135,123), (7, 7), 7)
+		self.rect.center = location
+	def set_location(self, location):
+		self.location = location
+		self.rect.center = location
+class Cover(pygame.sprite.Sprite):
+	def __init__(self, x, y, width, height):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface([width, height])
+		self.rect = self.image.get_rect()
+		self.width = width
+		self.height = height
+		self.rect.x = x
+		self.rect.y = y
+		self.location = (self.rect.x, self.rect.y)
+class Bullet(pygame.sprite.Sprite):
+	def __init__(self, origin, direction, magnitude):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface([2*3, 2*3], pygame.SRCALPHA)
+		self.rect = pygame.draw.circle(self.image, (127, 127, 127), (3, 3), 3)
+		self.damage = 10
+		o_x, o_y = origin
+		x, y = direction
+		self.location = (math.ceil(o_x + (7 + 3) * (x / math.sqrt(x**2 + y**2))), math.ceil(o_y + (7 + 3) * (y / math.sqrt(x**2 + y**2))))
+		self.rect.center = self.location
+		scale = magnitude / math.sqrt(x**2 + y**2)
+		self.velocity = (x * scale, y * scale)
+	def update(self):
+		v_x, v_y = self.velocity
+		self.rect.x += v_x
+		self.rect.y += v_y
+		self.location = self.rect.center
+		players_hit = pygame.sprite.spritecollide(self, players, False)
+		if len(players_hit) != 0:
+			for player in players_hit:
+				player.health = player.health - self.damage
+			self.kill()
+			return
+		covers_hit = pygame.sprite.spritecollide(self, covers, False)
+		if len(covers_hit) != 0:
+			self.kill()
+MAP_WIDTH = 2500
+MAP_HEIGHT = 2500
 covers_count = 100
 
 ServerSocket = socket.socket()
@@ -17,33 +69,55 @@ except socket.error as e:
 	print(str(e))
 
 #initialize map
-covers = []
-for i in range(covers_count):
-	covers.append((random.randrange(MAP_WIDTH), random.randrange(MAP_HEIGHT), random.randrange(40, 80), random.randrange(40, 80)))
+covers = pygame.sprite.Group()
+covers.add(Cover(0, 0, MAP_WIDTH, 250))
+covers.add(Cover(0, MAP_HEIGHT - 250, MAP_WIDTH, 250))
+covers.add(Cover(0, 0, 250, MAP_HEIGHT))
+covers.add(Cover(MAP_WIDTH - 250, 0, 250, MAP_HEIGHT))
+for i in range(100):
+	cover = Cover(random.uniform(0, MAP_WIDTH), random.uniform(0, MAP_HEIGHT), random.uniform(40, 80), random.uniform(40, 80))
+	while True:
+		if not pygame.sprite.spritecollide(cover, covers, False): break
+		cover.rect.x, cover.rect.y = random.uniform(0, MAP_WIDTH), random.uniform(0, MAP_HEIGHT)
+	cover.location = (cover.rect.x, cover.rect.y)
+	covers.add(cover)
 
-usernames = [None] * 100
-locations = [None] * 100
+players = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 print('Waiting for a Connection..')
 ServerSocket.listen(5)
-
+players_to_remove = []
 
 def threaded_client(connection):
 	global player_count
 	player_id = player_count
 	player_count += 1
-	connection.send(str.encode(str(covers)))
-	username = connection.recv(1024)
-	usernames[player_id] = username
-	location = (random.randrange(MAP_WIDTH), random.randrange(MAP_HEIGHT))
-	locations[player_id] = location
-	connection.send(str.encode(str([player_id, location])))
-	connection.send(str.encode(str((usernames, locations))))
+	connection.send(str.encode(str([(cover.location, cover.width, cover.height) for cover in covers.sprites()])))
+	username = connection.recv(64)
+	location = (random.uniform(250, MAP_WIDTH - 250), random.uniform(250, MAP_HEIGHT - 250))
+	connection.send(str.encode(str((player_id, location, 100))))
+	connection.recv(64)
+	players.add(Player(player_id, username, location))
+	data = {}
+	for player in players.sprites():
+		data[player.ID] = (player.username, player.location, player.health)
+	connection.send(str.encode(str(data)))
 	while True:
 		new_location = connection.recv(2048)
-		locations[player_id] = eval(new_location.decode('utf-8'))
 		if not new_location:
 			break
-		connection.send(str.encode(str((usernames, locations))))
+		new_location = eval(new_location.decode('utf-8'))
+		for player in players.sprites():
+			if player.ID == player_id:
+				player.set_location(new_location)
+			data[player.ID] = (player.username, player.location, player.health)
+		connection.send(str.encode(str(data)))
+		bullets.update()
+		bullet = connection.recv(1024)
+		if bullet.decode('utf-8') != "empty":
+			origin, direction, magnitude = eval(bullet.decode('utf-8'))
+			bullets.add(Bullet(origin, direction, magnitude))
+		connection.send(str.encode(str([bullet.location for bullet in bullets])))
 	connection.close()
 
 while True:
