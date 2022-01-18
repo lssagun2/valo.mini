2b03c19905dcd676c0c36f59943cbb5aa808e4ac
gdderije <71222985+gdderije@users.noreply.github.com>
2022-01-18 17:37:30 +0800

    choose character function

---
+++ entities.py
@@ -0,0 +1,144 @@
+import pygame
+import time
+import math
+import random
+from variables import *
+from pygame.locals import *
+from pygame import mixer
+
+#initialize Pygame
+pygame.init()
+clock = pygame.time.Clock()
+
+#create main display
+screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+logo = pygame.image.load('assets/logo/valo.png')
+pygame.display.set_caption('Valo.mini')
+pygame.display.set_icon(logo)
+display_surface = pygame.Surface((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+display_surface = display_surface.convert()
+MAP.convert()
+
+char_style1 = pygame.image.load('assets/characters/character1/walking1.png').convert_alpha()
+char_style2 = pygame.image.load('assets/characters/character2/walking1.png').convert_alpha()
+char_style3 = pygame.image.load('assets/characters/character3/walking1.png').convert_alpha()
+char_style1 = pygame.transform.scale(char_style1, (648, 648))
+char_style2 = pygame.transform.scale(char_style2, (648, 648))
+char_style3 = pygame.transform.scale(char_style3, (648, 648))
+
+#load images into dictionaries
+images = {"character1": None, "character2": None}
+for character in images.keys():
+	images[character] = {"walking": None, "ranged": None};
+	for state in images[character].keys():
+		directory = "assets/characters/" + character + "/" + state + ""
+		images[character][state] = [pygame.image.load(directory + str(i) + ".png") for i in range(1, 5)]
+
+class Player(pygame.sprite.Sprite):
+	def __init__(self, ID, username, location, health, speed, character = "character2", state = "walking"):
+		pygame.sprite.Sprite.__init__(self)
+		self.ID = ID
+		self.username = username
+		self.health = health
+		self.speed = speed
+		self.gun = Gun(30, 10, 10, 2000)
+		self.time_last_fired = 0
+		self.ammo = 1000
+		self.reloading = False
+		self.animation_count = 0
+		self.still = True
+		self.character = character
+		self.state = state
+		self.image = images[character][state][0]
+		self.mask = pygame.mask.from_surface(self.image)
+		self.rect = self.image.get_rect(center = location)
+		self.location = location
+
+	def update(self, location, health):
+		self.location = location
+		self.rect.center = location
+		self.health = health
+
+	def animate(self, mouse_position):
+		x, y = mouse_position
+		angle = (180 / math.pi) * (-math.atan2(y - DISPLAY_HEIGHT/2, x - DISPLAY_WIDTH/2))
+		if self.still:
+			self.image = pygame.transform.rotate(images[self.character][self.state][0], angle)
+		else:
+			self.image = pygame.transform.rotate(images[self.character][self.state][self.animation_count // 8], angle)
+		self.mask = pygame.mask.from_surface(self.image)
+		self.rect = self.image.get_rect()
+		self.rect.center = self.location
+		self.animation_count = (self.animation_count + 1) % 32
+
+	def reload(self):
+		if self.ammo > self.gun.clip_size - self.gun.ammo:
+			self.ammo -= self.gun.clip_size- self.gun.ammo
+			self.gun.ammo += self.gun.clip_size - self.gun.ammo
+		else:
+			self.gun.ammo += self.ammo
+			self.ammo = 0
+		self.reloading = False
+
+	def fire(self):
+		current_time = time.time()
+		if not self.reloading:
+			if self.gun.ammo > 0:
+				if current_time - self.time_last_fired > 1 / self.gun.fire_rate:
+					self.time_last_fired = current_time
+					self.gun.ammo -= 1
+					origin = self.location
+					x, y = pygame.mouse.get_pos()
+					direction = (x - DISPLAY_WIDTH / 2, y - DISPLAY_HEIGHT / 2)
+					return (origin, direction, self.gun.bullet_speed)
+			else:
+				if self.ammo > 0:
+					self.reloading = True
+					pygame.time.set_timer(RELOAD, self.gun.reload_time, loops = 1)
+		return " "
+class Cover(pygame.sprite.Sprite):
+	def __init__(self, location, width, height):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface([width, height])
+		self.mask = pygame.mask.from_surface(self.image)
+		self.rect = self.image.get_rect()
+		self.rect.x, self.rect.y = location
+class Bullet(pygame.sprite.Sprite):
+	def __init__(self, location, radius = 3):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface([2*radius, 2*radius], pygame.SRCALPHA)
+		self.rect = pygame.draw.circle(self.image, (0, 0, 0), (radius, radius), radius)
+		self.rect.center = location
+class Gun(pygame.sprite.Sprite):
+	def __init__(self, clip_size, fire_rate, bullet_speed, reload_time, full_auto = True):
+		pygame.sprite.Sprite.__init__(self)
+		self.ammo = clip_size
+		self.clip_size = clip_size
+		self.fire_rate = fire_rate
+		self.reload_time = reload_time
+		self.bullet_speed = bullet_speed
+		self.full_auto = full_auto
+	# def animate():
+class Button:
+    def __init__(self, x, y, image):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
+
+    def draw(self):
+        action = False
+        pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked == False
+
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ import pygame, cv2, sys
 from pygame.locals import *
 from pygame import mixer
 from game import *
+import entities
 
 def draw_text(surface, font, text, location, color):
 	# font = pygame.font.Font(font_name, size)
@@ -48,7 +49,8 @@ def request_username(surface):
 				exit()
 			if event.type == KEYDOWN:
 				if event.key == pygame.K_RETURN:
-					game(username)
+					# game(username)
+					request_char()
 				elif event.key == pygame.K_BACKSPACE:
 					username = username[0:-1]
 				elif event.key == K_MINUS:
@@ -61,51 +63,35 @@ def request_username(surface):
 		font = pygame.font.Font("assets/font/8-BIT WONDER.TTF", 40)
 		draw_text(surface, font, username, (DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 + 30), WHITE)
 		pygame.display.flip()
-# def character_style(surface):
-# 	char_style1 = pygame.image.load('assets/characters/character1/walking1.png')
-# 	char_style1 = pygame.transform.scale(char_style1, (648, 648))
-# 	char_style2 = pygame.image.load('assets/characters/character2/walking1.png')
-# 	char_style2 = pygame.transform.scale(char_style2, (648, 648))
-# 	char_style3 = pygame.image.load('assets/characters/character3/walking1.png')
-# 	char_style3 = pygame.transform.scale(char_style3, (648, 648))
-# 	surface.blit(char_style1, (-150, 30))
-# 	surface.blit(char_style2, (300, 10))
-# 	surface.blit(char_style3, (700, -10))
-#
-# 	character1 = char_style1.get_rect()
-# 	character2 = char_style2.get_rect()
-# 	character3 = char_style3.get_rect()
-#
-# 	pos = pygame.mouse.get_pos()
-#
-# 	clicked = False
-#
-# 	if character1.collidepoint(pos):
-# 		if pygame.mouse.get_pressed()[0] == 1 and clicked == False:
-# 			self.clicked = True
-# 			print('Character 1')
-# 	if pygame.mouse.get_pressed()[0] == 0:
-#             self.clicked == False
-#
-# 	# if self.rect.collidepoint(pos):
-# 	# 	if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
-# 	# 		self.clicked = True
-# 	# 		action = True
-# 	# if pygame.mouse.get_pressed()[0] == 0:
-# 	# 	self.clicked == False
-#
-# 	run = True
-# 	while run:
-# 		if char_style1 == True:
-# 			print('You chose Character 1')
-# 		elif char_style2 == True:
-# 			print('You chose Character 2')
-# 		elif char_style3 == True:
-# 			print('You chose Character 3')
-# 		for event in pygame.event.get():
-# 			if event.type == pygame.QUIT():
-# 				pygame.quit()
-# 			pygame.display.update()
+
+def request_char():
+	font = pygame.font.Font("assets/font/8-BIT WONDER.TTF", 30)
+	character1 = entities.Button(-150, 80, char_style1)
+	character2 = entities.Button(300, 60, char_style2)
+	character3 = entities.Button(700, 40, char_style3)
+
+	run = True
+	while run:
+		if character1.draw() == True:
+			# action
+			print('You chose Character 1')
+		elif character2.draw() == True:
+			# action
+			print('You chose Character 2')
+		elif character3.draw() == True:
+			# action
+			print('You chose Character 3')
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				exit()
+		screen.blit(MENU_BACKGROUND, (0, 0))
+		draw_text(screen, font, 'CHOOSE YOUR CHARACTER', (DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 - 100), WHITE)
+		screen.blit(char_style1, (-150, 80))
+		screen.blit(char_style2, (300, 60))
+		screen.blit(char_style3, (700, 40))
+		pygame.display.flip()
+
 def credits(surface):
 	options = [('Gisselle Derije', (DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 + 40)), ('Leandrei Sagun', (DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 + 100))]
 	selected = 0
